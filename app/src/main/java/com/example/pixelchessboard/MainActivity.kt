6ba400d92b7cbbaf35fafd51fa1464dc36863fc0
bgarza9188 <bgarza9188@gmail.com>
2026-05-21 21:37:55 -0500

    Added click listener

--- app/src/main/java/com/example/pixelchessboard/MainActivity.kt
+++ app/src/main/java/com/example/pixelchessboard/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,7 +14,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,12 +46,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ChessBoard(modifier: Modifier = Modifier) {
     val files = 'a'..'h'
-    val ranks = 1..8
+    val ranks = 8 downTo 1
+    var selectedSquare by remember { mutableStateOf(Pair('a', 1)) }
     Column(
-        modifier = modifier.fillMaxSize().background(Color.DarkGray),
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text("${selectedSquare.first}${selectedSquare.second}")
         ranks.forEachIndexed { i, rank ->
             Row {
                 files.forEachIndexed { j, file ->
@@ -58,6 +66,9 @@ fun ChessBoard(modifier: Modifier = Modifier) {
                         modifier = Modifier
                             .size(42.dp)
                             .background(color)
+                            .clickable {
+                                selectedSquare = Pair(file, rank)
+                            }
                     )
                 }
             }
